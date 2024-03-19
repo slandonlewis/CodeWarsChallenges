a2860597056c363be11ffcbf2793979cdea008c0
Seth Lewis <slandonlewis@gmail.com>
2024-03-19 08:07:06 -0500

    set up javascript structure

---
+++ Unique In Order/index.js
@@ -0,0 +1,20 @@
+/*
+    DIRECTIONS:
+    Write a function that takes a sequence as an argument and
+    returns a list of items without any matching elements next to
+    each other, preserving the original order of elements.
+*/
+
+const uniqueInOrder = (iterable) => {
+    //your code here - remember iterable can be a string or an array
+
+}
+
+// TEST VARIABLES
+const test1 = 'AAAABBBCCDAABBB'
+const test2 = 'ABBCcAD'
+const test3 = [1,2,2,3,3]
+// FUNCTION TESTS
+console.log(uniqueInOrder(test1))
+console.log(uniqueInOrder(test2))
+console.log(uniqueInOrder(test3))

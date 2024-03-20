5df401277df4c49743bef9d1aeac331fc97cbbb0
Seth Lewis <slandonlewis@gmail.com>
2024-03-20 08:28:21 -0500

    solved

--- Unique In Order/index.js
+++ Unique In Order/index.js
@@ -5,9 +5,31 @@
     each other, preserving the original order of elements.
 */
 
+// reusable filter function
+const createModifiedArrays = (arrayToBeModified) => {
+    const modified = arrayToBeModified.filter((element, index, array) => {
+        return element !== array[index + 1]
+    })
+    return modified
+}
+
 const uniqueInOrder = (iterable) => {
     //your code here - remember iterable can be a string or an array
-
+    let filtered = []
+    // check for string or array
+    if (typeof iterable === 'string') {
+        // return 'string'
+        let splitString = iterable.split('')
+        // console.log(typeof splitString)
+        filtered = createModifiedArrays(splitString)
+        return filtered
+    } else if (Array.isArray(iterable)) {
+        // return 'array'
+        filtered = createModifiedArrays(iterable)
+        return filtered        
+    } else {
+        return 'please use a string or array'
+    }
 }
 
 // TEST VARIABLES
@@ -18,3 +40,11 @@ const test3 = [1,2,2,3,3]
 console.log(uniqueInOrder(test1))
 console.log(uniqueInOrder(test2))
 console.log(uniqueInOrder(test3))
+
+/*
+    THINKING SPACE:
+    1. If the item is a string, split the string. If not, proceed.
+    2. Filter the array so when b === a, b is skipped. This
+        should remove consecutive values.
+    3. Return the filtered array
+*/
